ce55688469b6e21545765815e31ac1c6f8a153d4
Tim Finnell <tim.finnell@gmail.com>
2012-03-29 22:25:18 -0700

    determine if sourceline is comment or codeline

---
+++ tests/sourceline_test.php
@@ -0,0 +1,25 @@
+<?php
+require_once('simpletest/autorun.php');
+require_once('../classes/sourceline.php');
+
+class TestSourcelineObject extends UnitTestCase {
+    function testSourceLineHasLineType() {
+        $line = "puts 'hello'";
+        $sourceline = new Sourceline($line);
+        $this->assertNotNull($sourceline->linetype);
+    }
+
+    function testSourceLineIsComment() {
+        $line = '// this is a line comment';
+        $sourceline = new Sourceline($line);
+        $p = '/comment/';
+        $this->assertPattern($p, $sourceline->linetype);
+    }
+
+    function testSourceLineIsCodeline() {
+        $line = 'echo "this is a php codeline"';
+        $sourceline = new Sourceline($line);
+        $p = 'codeline';
+        $this->assertEqual($sourceline->linetype, $p);
+    }
+}
