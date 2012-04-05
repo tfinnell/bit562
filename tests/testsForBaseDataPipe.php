0f514ef1c34c83f5aac7a2b53e59b7f7fb77dc9a
Tim Finnell <tim.finnell@gmail.com>
2012-04-05 15:43:14 -0700

    test for db credentials, used $GLOBALS array to access credentials

--- tests/testsForBaseDataPipe.php
+++ tests/testsForBaseDataPipe.php
@@ -17,13 +17,29 @@ class BaseDataPipeTester extends UnitTestCase {
             '/../php/baseDataPipe.php'));
     }
 
-    function testBaseDataPipeObject() {
-        $db_dsn = "mysql:host={$db_host};dbname={$db_database}";
-        $databaseManager = new DBManager(
-            $db_dsn, $db_username, $db_password);
-        $tableMapManager = new TableMapManager($databaseManager);
-        $datapipe = new BaseDataPipe($tableMapManager, $databaseManager);
+    function testDataBaseCredentials() {
+        $this->assertNotNull($GLOBALS['db_username']);
+        $this->assertNotNull($GLOBALS['db_password']);
+        $this->assertNotNull($GLOBALS['db_database']);
     }
+
+    function testDataBaseManager() {
+        $db_dsn = "mysql:host={$GLOBALS['db_host']};".
+            "dbname={$GLOBALS['db_database']}";
+        $databaseManager = new DBManager(
+            $db_dsn, $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $databaseManager->open();
+        $this->assertEqual(get_class($databaseManager), DBManager);
+    }
+
+#    function testBaseDataPipeObject() {
+#        $db_dsn = "mysql:host={$db_host};dbname={$db_database}";
+#        $databaseManager = new DBManager(
+#            $db_dsn, $db_username, $db_password);
+#        $databaseManager->open();
+#        $tableMapManager = new TableMapManager($databaseManager);
+#        $datapipe = new BaseDataPipe($tableMapManager, $databaseManager);
+#    }
 
 }
 
