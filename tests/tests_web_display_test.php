6f319b960669517d4f7352dac8fbf693f414d1e9
Tim Finnell <tim.finnell@gmail.com>
2012-04-06 05:45:40 -0700

    created tests for dbmanager object, credentials, table existence and for http server serving the page

--- tests/tests_web_display_test.php
+++ tests/tests_web_display_test.php
@@ -1,8 +1,46 @@
 <?php
 
 require_once(dirname(__FILE__) . '/simpletest/autorun.php');
+require_once(dirname(__FILE__) . '/simpletest/web_tester.php');
+require_once(dirname(__FILE__).'/../php/db_login.php');
+require_once(dirname(__FILE__).'/../php/DBManager.php');
 
-class TestOfTestsWebDisplay() extends UnitTestCase {
-    function testDataBaseResponse() {
+class TestOfTestsWebDisplay extends UnitTestCase {
+    function testDatabaseCredentials() {
+        $this->assertNotNull($GLOBALS['db_username']);
+        $this->assertNotNull($GLOBALS['db_password']);
+        $this->assertNotNull($GLOBALS['db_database']);
+    }
+
+    function testDataBaseManager() {
+        $db_dsn = "mysql:host={$GLOBALS['db_host']};".
+            "dbname={$GLOBALS['db_database']}";
+        $databaseManager = new DBManager(
+            $db_dsn, $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $this->assertIsA($databaseManager, DBManager);
+    }
+
+    function testTestTableExistence() {
+        $db_dsn = "mysql:host={$GLOBALS['db_host']};".
+            "dbname={$GLOBALS['db_database']}";
+        $dbManager = new DBManager(
+            $db_dsn, $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $dbManager->open();
+        $dbManager->test('test string');
+        $res = $dbManager->execute(
+            'SELECT COUNT(*)'.
+            'FROM information_schema.tables'.
+            'WHERE table_schema = '.$GLOBALS['db_database'].' '.
+            'AND table_name = \'test\''
+        );
+        $this->assertEqual($res, 1);
     }
 }
+
+class TestOfTestWebDisplayWebTests extends WebTestCase {
+    function testPageExists() {
+        $this->get('http://localhost/bit562/php/dbtest.php');
+        $this->assertResponse(200);
+    }
+}
+
