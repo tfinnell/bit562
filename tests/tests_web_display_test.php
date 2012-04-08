86fea2a8972f92d83c28d7e89b11f18ff600088c
Tim Finnell <tim.finnell@gmail.com>
2012-04-07 17:12:56 -0700

    moved to setUp()

--- tests/tests_web_display_test.php
+++ tests/tests_web_display_test.php
@@ -6,19 +6,29 @@ require_once(dirname(__FILE__).'/../php/db_login.php');
 require_once(dirname(__FILE__).'/../php/DBManager.php');
 
 class TestOfTestsWebDisplay extends UnitTestCase {
-    function testTestTableExistence() {
+    private $DBManager;
+
+    function setUp() {
         $db_dsn = "mysql:host={$GLOBALS['db_host']};".
             "dbname={$GLOBALS['db_database']}";
-        $dbManager = new DBManager(
+        $this->DBManager = new DBManager(
             $db_dsn, $GLOBALS['db_username'], $GLOBALS['db_password']);
-        $dbManager->open();
-        $res = $dbManager->execute(
+    }
+
+    function testTestTableExistence() {
+        $this->DBManager->open();
+        $res = $this->DBManager->execute(
             'SELECT COUNT(*) '.
             'FROM information_schema.tables '.
             "WHERE table_schema = '{$GLOBALS['db_database']}' ".
             "AND table_name = 'test';"
         );
         $this->assertEqual($res, 1);
+    }
+
+    function testDBManagerReturnsAllTests() {
+        $this->DBManager->open();
+        $res = $this->DBManager->getAll
     }
 }
 
