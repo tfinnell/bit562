b888d7356375508ca753176dda084832bed40877
cmgriffing <cmgriffing@gmail.com>
2012-04-10 04:27:13 -0700

    refactored the rest of the toggle tests on DBManager.  The tried using namespacing to mock the PDO object.  Constructor currently not being overridden properly

--- tests/DBManagerTests.php
+++ tests/DBManagerTests.php
@@ -1,16 +1,20 @@
 <?php
+// Using namespaces in this way allows "monkey patching" of classes and methods.
+// Since namespacing is restricted to the first line of the script, 
+//   it means that different mock outputs need to be refactored into separate files
+namespace NoMockPDONeeded;
+
 //* This file is for Units Tests of the DBManager class.
 
 if(!class_exists('UnitTestCase')) include('simpletest/autorun.php');
 if(!class_exists('DBManager')) include('../php/DBManager.php');
 
-class DBManagerTests extends UnitTestCase {
+class DBManagerTests extends \UnitTestCase {
     
     public function testConstructor() {
     
-        
-        $DBManager = new DBManager("arbitrary string", "user", "pass");
-        $DBMReflection = new ReflectionObject($DBManager);
+        $DBManager = new \DBManager("arbitrary string", "user", "pass");
+        $DBMReflection = new \ReflectionObject($DBManager);
         
         $this->assertTrue($DBMReflection->hasProperty("db_dsn"), "The DBManager should have a property called db_dsn");
         if($DBMReflection->hasProperty("db_dsn")) {
@@ -18,36 +22,42 @@ class DBManagerTests extends UnitTestCase {
             $propertyDBDSN->setAccessible(true);
             $this->assertEqual($propertyDBDSN->getValue($DBManager), "arbitrary string", "The dsn should be properly set using arbitrary strings.  If any validation is placed on the input of the dsn, then this test must be modified accordingly.");
         }
-        /*
+        
         $this->assertTrue($DBMReflection->hasProperty("db_username"), "The DBManager should have a property called db_username");
-        if($DBMReflection->hasProperty("db_username"))
-        $this->assertEqual($DBMReflection->getProperty("db_username"), "user", "The user name should be properly set using arbitrary strings");
+        if($DBMReflection->hasProperty("db_username")) {
+            $propertyUserName = $DBMReflection->getProperty("db_username");
+            $propertyUserName->setAccessible(true);
+            $this->assertEqual($propertyUserName->getValue($DBManager), "user", "The user name should be properly set using arbitrary strings");
+        }
         
         $this->assertTrue($DBMReflection->hasProperty("db_password"), "The DBManager should have a property called db_password");
-        if($DBMReflection->hasProperty("db_password"))
-        $this->assertEqual($DBMReflection->getProperty("db_password"), "pass", "The pass should be properly set using arbitrary strings");
-        */
+        if($DBMReflection->hasProperty("db_password")) {
+            $propertyPassword = $DBMReflection->getProperty("db_password");
+            $propertyPassword->setAccessible(true);
+            $this->assertEqual($propertyPassword->getValue($DBManager), "pass", "The pass should be properly set using arbitrary strings");
+        }
+        
         // The DBManager does not have any type of data validation on the constructor.
         //  It is currently uncertain what should happen on improper input.  The subsequent tests assume the constructor would fail and return null.
-        $arbitraryObject = new stdClass();
-        $DBManager = new DBManager($arbitraryObject, "user", "pass");
+        $arbitraryObject = new \stdClass();
+        $DBManager = new \DBManager($arbitraryObject, "user", "pass");
         $this->assertNull($DBManager, "An object instead of a string was passed to the db_dsn property which should result in a null object");
-        $DBManager = new DBManager("arbitrary string", $arbitraryObject, "pass");
+        $DBManager = new \DBManager("arbitrary string", $arbitraryObject, "pass");
         $this->assertNull($DBManager, "An object instead of a string was passed to the db_username property which should result in a null object");
-        $DBManager = new DBManager("arbitrary string", "user", $arbitraryObject);
+        $DBManager = new \DBManager("arbitrary string", "user", $arbitraryObject);
         $this->assertNull($DBManager, "An object instead of a string was passed to the db_password property which should result in a null object");
         
         
     }
     
     public function testGetDBTestsWithEmptyDSN() {
-        $DBManager = new DBManager("", "user", "pass");
+        $DBManager = new \DBManager("", "user", "pass");
         $getTestsResult = $DBManager->getDBTests();
         $this->assertNull($getTestsResult, "This should be null because the DBManager has not opened its connection yet.");
     }
     
     public function testGetDBTestsWithProperDSN() {
-        $DBManager = new DBManager("mysql:host={$GLOBALS['db_host']};dbname={$GLOBALS['db_database']}", $GLOBALS['db_username'], $GLOBALS['db_password']);
+        $DBManager = new \DBManager("mysql:host={$GLOBALS['db_host']};dbname={$GLOBALS['db_database']}", $GLOBALS['db_username'], $GLOBALS['db_password']);
         $getTestsResult = $DBManager->getDBTests();
         $this->assertTrue(is_array($getTestsResult), "The response should be an array.");
         
@@ -55,7 +65,7 @@ class DBManagerTests extends UnitTestCase {
     
     public function testScrub() {
         // DBManager constructor arguments don't matter for this test 
-        $DBManager = new DBManager("", "user", "pass");
+        $DBManager = new \DBManager("", "user", "pass");
         $scrubbed = $DBManager->scrub("'");
         $this->assertEqual($scrubbed, "%pos;", "Should scrub a lone apostrophe into %pos;");
         $scrubbed = $DBManager->scrub("");
@@ -63,7 +73,7 @@ class DBManagerTests extends UnitTestCase {
     }
     
     public function testDeScrub() {
-        $DBManager = new DBManager("", "user", "pass");
+        $DBManager = new \DBManager("", "user", "pass");
         $deScrubbed = $DBManager->deScrub("%pos;");
         $this->assertEqual($deScrubbed, "'", "Should scrub a lone %pos; into '");
         $deScrubbed = $DBManager->deScrub("");
@@ -73,23 +83,65 @@ class DBManagerTests extends UnitTestCase {
     
     //I realized assertOn and positiveTest are private, so these two test methods are meaningless right now.
     public function testAssertToggle() {
-        $DBManager = new DBManager("", "user", "pass");
-        $DBMReflection = newReflectionObject($DBManager);
-        $this->assertFalse($DBMReflection->getProperty("assertOn"), "The constructor should set assertOn to false.");
+        $DBManager = new \DBManager("", "user", "pass");
+        $DBMReflection = new \ReflectionObject($DBManager);
+        $propertyAssertOn = $DBMReflection->getProperty("assertOn");
+        $propertyAssertOn->setAccessible(true);
+        $this->assertFalse($propertyAssertOn->getValue($DBManager), "The constructor should set assertOn to false.");
         $DBManager->assertToggle();
-        $this->assertTrue($DBMReflection->getProperty("assertOn"), "This should be true since the variable declaration starts out false.");
+        $this->assertTrue($propertyAssertOn->getValue($DBManager), "This should be true since the variable declaration starts out false.");
         $DBManager->assertToggle();
-        $this->assertFalse($DBMReflection->getProperty("assertOn"), "This is verifying that the toggle works both ways, assuming the previous test passed.");
+        $this->assertFalse($propertyAssertOn->getValue($DBManager), "This is verifying that the toggle works both ways, assuming the previous test passed.");
     }
-    /*
+    
     public function testPositiveTestToggle() {
-        $DBManager = new DBManager("", "user", "pass");
+        $DBManager = new \DBManager("", "user", "pass");
+        $DBMReflection = new \ReflectionObject($DBManager);
+        $propertyPositiveTest = $DBMReflection->getProperty("positiveTest");
+        $propertyPositiveTest->setAccessible(true);
+        $this->assertTrue($propertyPositiveTest->getValue($DBManager), "This should be true at object creation.");
         $DBManager->positiveTestToggle();
-        $this->assertFalse($DBManager->positiveTest, "This should be true since the variable declaration starts out True.");
+        $this->assertFalse($propertyPositiveTest->getValue($DBManager), "This should be false since the variable declaration starts out true.");
         $DBManager->positiveTestToggle();
-        $this->assertTrue($DBManager->positiveTest, "This is verifying that the toggle works both ways, assuming the previous test passed.");
-    }*/
+        $this->assertTrue($propertyPositiveTest->getValue($DBManager), "This is verifying that the toggle works both ways, assuming the previous test passed.");
+    }
    
 }
+
+namespace ExpectedResults;
+
+// I am trying to override the PDO object so that we can unit test the methods that need a database connection.
+class PDO extends \PDO {
+
+    public function __construct($a, $b, $c) {
+        //empty
+        return;
+    }
+
+    public function query($sql) {
+        return 'some text';
+        
+        /* pseudo-code for more accurate input/response cycle
+            if( $sql matches $pattern ) {
+                return realistic result
+            }
+        */       
+    }
+}
+use ExpectedResults\PDO as PDO;
+class DBManagerTestsWithExpectedResults extends \UnitTestCase {
+    
+    function testOpenAndExecute() {
+        $DBManager = new \DBManager("","","");
+        $DBManager->open();
+        $DBMReflection = new \ReflectionObject($DBManager);
+        $connectionClass = $DBMReflection->getProperty("connection")->class;
+        $this->assertIsA($connectionClass, 'PDO', "after the open method, the connection should be a PDO object.");
+        $result = $DBManager->execute("some sql");
+        $this->assertEqual($result, "some sql", "This test needs to be changed.");
+    }
+    
+}
+
 
 ?>
